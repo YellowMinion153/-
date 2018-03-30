92cae00c88cf2c4c56690429a272adea1921931d
YellowMinion153 <troll.boxxd@mail.ru>
2018-03-30 23:54:29 +0300

    s

--- dayn.js
+++ dayn.js
@@ -166,11 +166,16 @@ bot.on('message', msg => {
 		return;
 	};
 	
-	if(h === 12 + m === 00){
-		bot.channels.get("429349387139809281").send('```*собирает повстанцев*```')
+	if(ignoreCase.equals(msg.content, '```*собирает повстанцев*```')){
+		m = m + 1;
 		return;
-	};
-	
+	}else{
+		if(h === 12, m === 00){
+			bot.channels.get('429349387139809281').send('```*собирает повстанцев*```')
+		}
+		return;
+	}
+		
     if(ignoreCase.equals(msg.content, 'g!heart')){
         msg.channel.send({
             embed: {
