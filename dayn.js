6d76b3a3cd5a60a040e71491d0f15e6f0f2b591e
YellowMinion153 <troll.boxxd@mail.ru>
2018-03-30 18:59:48 +0300

    s

--- dayn.js
+++ dayn.js
@@ -182,6 +182,7 @@ formateTime = function(){
         formattedD = '0' + d;
     else
         formattedD = d;
+	client.user.setPresence({ game: { name: 'Вьетнам   ' + formattedD + ':' + formattedH + ':' + formattedM, type: 1 } });
 };
 
 logTime = function () {
