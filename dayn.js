682c931b8a60c30db94bb5b6f553843362f3a5bc
YellowMinion153 <troll.boxxd@mail.ru>
2018-03-31 21:21:27 +0300

    s

--- dayn.js
+++ dayn.js
@@ -166,7 +166,7 @@ bot.on('message', msg => {
 		return;
 	};
 
-		if(formattedH = '12' + ':' + formattedM = '00' === 12:00){
+		if(h === 12, m === 00){
 			bot.channels.get('429349387139809281').send('```*собирает повстанцев*```')
       return;
 		};
