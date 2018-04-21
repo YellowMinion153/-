4087cfaea06312cec41a84fcc40bc9ef341910b6
YellowMinion153 <troll.boxxd@mail.ru>
2018-04-21 13:23:28 +0300

    s

--- dayn.js
+++ dayn.js
@@ -168,7 +168,6 @@ bot.on('message', msg => {
 
 	if(h === 12, m === 00){
 	    client.channels.get('423591720094007308').send('``*собирает отряды гуков*``')
-        return;
     };
 
     if(ignoreCase.equals(msg.content, 'g!heart')){
